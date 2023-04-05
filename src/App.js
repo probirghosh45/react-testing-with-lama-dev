22ba7231e8c8fbb236d452a35ccf2ec09e5780a6
probir ghosh <probirghosh@outlook.com>
2023-04-05 19:06:32 +0600

    list items tested successfully :tada:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -17,6 +17,11 @@ function App() {
         >
           Learn React
         </a>
+        <ul>
+          <li>Arjun</li>
+          <li>Krishna</li>
+          <li>Judishthir</li>
+        </ul>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders list Items', () => {
+  render(<App />);
+  const listItems = screen.getAllByRole('listitem');
+  expect(listItems).toHaveLength(3);
+});
